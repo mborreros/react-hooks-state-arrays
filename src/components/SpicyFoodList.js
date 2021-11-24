c0e5532c9082c5e31a9d6a8a6171048c0bf335e7
Maya Borrero <mlbsindia@gmail.com>
2021-11-24 17:03:38 -0500

    commit tree 231506f6c6bb9a43b44d113c0b32cb12c36bbdc3

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,3 +1,7 @@
+// using a state variable to hold an array of spicy foods;
+// using that array to display each spicy food as an <li>; and
+// adding a new spicy food to the array when the button is clicked.
+
 import React, { useState } from "react";
 import { spicyFoods, getNewSpicyFood } from "../data";
 
@@ -6,13 +10,18 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newSpicyFoods = [...foods, newFood]
+    setFoods(newSpicyFoods)
   }
+
+  let foodElements = (foods.map((spicyFood) => <li key={spicyFood.id}>{spicyFood.name}</li>))
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>
+        {foodElements}
+      </ul>
     </div>
   );
 }
